51a2f6b447f60e7504848c855ea406587bf00c3c
KrLite <68179735+KrLite@users.noreply.github.com>
2023-02-17 19:18:32 +0800

    knowledges

--- src/main/java/net/krlite/knowledges/Knowledges.java
+++ src/main/java/net/krlite/knowledges/Knowledges.java
@@ -1,10 +1,10 @@
-package net.krlite.modid;
+package net.krlite.knowledges;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class Knowledges implements ModInitializer {
 	public static final String MOD_ID = "modid";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
