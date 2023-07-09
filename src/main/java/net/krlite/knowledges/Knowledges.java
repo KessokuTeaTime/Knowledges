c9989a5ada98be9436e8e125263b025caad5a3cb
KrLite <68179735+KrLite@users.noreply.github.com>
2023-07-09 11:18:29 +0800

    POG

--- src/main/java/net/krlite/knowledges/Knowledges.java
+++ src/main/java/net/krlite/knowledges/Knowledges.java
@@ -1,6 +1,7 @@
 package net.krlite.knowledges;
 
 import net.fabricmc.api.ModInitializer;
+import net.krlite.equator.visual.animation.Interpolation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +9,17 @@ public class Knowledges implements ModInitializer {
 	public static final String NAME = "Knowledges", ID = "knowledges";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
+	private static final Interpolation blockBreakingProgress = new Interpolation(0, 0, 45);
+
 	@Override
 	public void onInitialize() {
+	}
+
+	public static double blockBreakingProgress() {
+		return blockBreakingProgress.value();
+	}
+
+	public static void blockBreakingProgress(double progress) {
+		blockBreakingProgress.targetValue(progress);
 	}
 }
