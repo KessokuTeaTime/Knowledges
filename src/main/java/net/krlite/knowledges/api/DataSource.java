f7a7b0ea17dfdb47858961c2237e887cd98fca9a
KrLite <68179735+KrLite@users.noreply.github.com>
2024-01-10 17:32:13 +0800

    Update DataSource.java

--- src/main/java/net/krlite/knowledges/api/DataSource.java
+++ src/main/java/net/krlite/knowledges/api/DataSource.java
@@ -8,10 +8,6 @@ import java.util.function.Function;
 public interface DataSource<K extends Knowledge, S extends DataSource<K, S>> {
     Class<K> target();
 
-    default List<S> listeners() {
-        return Knowledges.DATA.availableListenersFromSource(this);
-    }
-
     interface Functional<S extends DataSource<?, S>> {
         Function<List<S>, S> function();
     }
